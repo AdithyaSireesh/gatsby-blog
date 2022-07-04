8a7e02af53ac06f9129c4a897828e15f5f670920
Adithya Sireesh <adithyasireesh@Mark-Zuckerberg.local>
2022-07-04 20:20:09 +0100

    Finished Gatsby Tutorial Part 3

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,8 @@ module.exports = {
     title: `gatsby-blog-site`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+  ],
 }
